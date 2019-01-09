969d1f3e9950f2efc1987bda87246887506ca48b
Niklaus Giger <niklaus.giger@member.fsf.org>
2019-01-10 00:00:47 +0100

    [14789] Fixing Artikelstamm-import with fixed wait times

--- save_run_from_scratch.sh
+++ save_run_from_scratch.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -v
 DESTINATION=${1:-~/elexis/rcptt/rcptt_result.h2.db}
 mkdir -p `dirname ${DESTINATION}`
+logger "Saved:  found:  `ls -l /tmp/elexis*h2.db`"
 cp -pv /tmp/elexis*h2.db ${DESTINATION}
 logger "Saved: $0 ${DESTINATION}"
