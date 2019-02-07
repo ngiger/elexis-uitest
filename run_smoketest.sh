e33558c4b83b7dbc63d5bc551d496b109f0d29f1
Niklaus Giger <niklaus.giger@member.fsf.org>
2019-02-07 09:14:21 +0100

    [14789] Fixed BasicTest, added BasicPrinting

--- run_smoketest.sh
+++ run_smoketest.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -v
 logger "cron: $0 started"
 cd `dirname $0`
-xvfb-run -a mvn -V clean verify  -f RunFromScratch/pom.xml -Dsuite2run=SmokeTestSuite  >> `basename --suffix=.sh $0`.log 2>&1
+xvfb-run -a mvn -V clean verify  -f RunFromScratch/pom.xml -Dsuite2run=SmokeTestSuite  2>&1 | tee `basename --suffix=.sh $0`.log
