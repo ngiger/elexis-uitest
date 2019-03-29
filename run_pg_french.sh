67692e4907b17e86da614dce02eb8a724212281f
Niklaus Giger <niklaus.giger@member.fsf.org>
2019-03-29 18:15:50 +0100

    [14789] Run Smoketest in french, too

--- run_pg_french.sh
+++ run_pg_french.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -v
 logger "cron: $0 started"
 cd `dirname $0`
-xvfb-run -a mvn -V clean install  -Dlanguage=fr -f RunFromScratch/pom.xml 2>&1 | tee `basename --suffix=.sh $0`.log
+xvfb-run -a mvn -V clean install  -Dlanguage=fr -Dsuite2run=SmokeTestSuite -f postgresql/pom.xml 2>&1 | tee `basename --suffix=.sh $0`.log
