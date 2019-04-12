a93239d32bcc50f6a940158e1a7ba3ba7c4f4628
Niklaus Giger <niklaus.giger@member.fsf.org>
2019-04-12 17:36:53 +0200

    [14789] Cleanup .travis, shell scripts launcher

--- run_FromScratch.sh
+++ run_FromScratch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -v
 logger "cron: $0 started"
 cd `dirname $0`
-xvfb-run -a --server-args="-screen 0 1280x1024x24" mvn -V clean verify  -f RunFromScratch/pom.xml -Dsuite2run=SmokeTestSuite  2>&1 | tee `basename --suffix=.sh $0`.log
+xvfb-run -a --server-args="-screen 0 1280x1024x24" mvn -V clean verify  -f RunFromScratch/pom.xml -Dsuite2run=QuickTestSuite  2>&1 | tee `basename --suffix=.sh $0`.log
