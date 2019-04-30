d295e12ed6c4bcd18f0097b790faa396aded45f4
Niklaus Giger <niklaus.giger@member.fsf.org>
2019-04-30 09:24:49 +0200

    [14789] Dump created h2 base after the tests

--- run_h2.sh
+++ run_h2.sh
@@ -1,4 +1,8 @@
 #!/bin/bash -v
 logger "cron: $0 started"
 cd `dirname $0`
+# other useful definitions are:
+# -DautExplicit=/opt/rm/elexis-master/elexis-3-core/ch.elexis.core.p2site/target/products/ch.elexis.core.application.ElexisApp-[classifier].zip
+# -Dsuite2run=QuickTestSuite
+# -DuseBranch=f111
 xvfb-run -a mvn -V clean install -f h2/pom.xml 2>&1 | tee `basename --suffix=.sh $0`.log
