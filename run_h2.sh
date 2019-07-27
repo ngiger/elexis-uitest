b690b9b1053df54a96177c35f8c4b9731e97808f
Niklaus Giger <niklaus.giger@member.fsf.org>
2019-07-27 12:14:26 +0200

    [15471] Start openbox

--- run_h2.sh
+++ run_h2.sh
@@ -5,4 +5,4 @@ cd `dirname $0`
 # -DautExplicit=/opt/rm/elexis-master/elexis-3-core/ch.elexis.core.p2site/target/products/ch.elexis.core.application.ElexisApp-[classifier].zip
 # -Dsuite2run=QuickTestSuite
 # -DuseBranch=f111
-xvfb-run -a mvn -V clean install -f h2/pom.xml 2>&1 | tee `basename --suffix=.sh $0`.log
+xvfb-run -a openbox --startup "mvn -V clean install -f $PWD/h2/pom.xml" 2>&1 | tee `basename --suffix=.sh $0`.log
--- run_mysql.sh
+++ run_mysql.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -v
 logger "cron: $0 started"
 cd `dirname $0`
-xvfb-run -a mvn -V clean install -f mysql/pom.xml 2>&1 | tee `basename --suffix=.sh $0`.log
+xvfb-run -a openbox --startup "mvn -V clean install -f $PWD/mysql/pom.xml" 2>&1 | tee `basename --suffix=.sh $0`.log
