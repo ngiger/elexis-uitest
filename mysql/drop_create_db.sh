6f1ff3d872644feb6f6dffe024376660723cecc0
Niklaus Giger <niklaus.giger@member.fsf.org>
2020-11-21 13:02:27 +0100

    [19620] Dump DB and schemas for all DBs. DB-names have language extension

--- mysql/drop_create_db.sh
+++ mysql/drop_create_db.sh
@@ -3,5 +3,5 @@ language=${1:-de}
 mysqlHost=${2:-127.0.0.1}
 mysqlPort=${3:-3306}
 echo $0: language ${language}  mysqlHost ${mysqlHost} mysqlPort ${mysqlPort}
-/usr/bin/env mysql --verbose --host=${mysqlHost} --port=${mysqlPort} --user=elexis --password=elexisTest --execute="drop database if exists elexis_rcptt;"
-/usr/bin/env mysql --verbose --host=${mysqlHost} --port=${mysqlPort} --user=elexis --password=elexisTest --execute="create database elexis_rcptt;"
+/usr/bin/env mysql --verbose --host=${mysqlHost} --port=${mysqlPort} --user=elexis --password=elexisTest --execute="drop database if exists elexis_rcptt_${language};"
+/usr/bin/env mysql --verbose --host=${mysqlHost} --port=${mysqlPort} --user=elexis --password=elexisTest --execute="create database elexis_rcptt_${language};"
--- postgresql/drop_create_db.sh
+++ postgresql/drop_create_db.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 export PGPASSWORD=elexisTest
+export language=${1:-de}
 # echo $0: language ${language}  mysqlHost ${mysqlHost} mysqlPort ${mysqlPort}
-/usr/bin/env psql --host=localhost --user=elexis --echo-queries --command="drop database if exists elexis_rcptt_$1;"
-/usr/bin/env psql --host=localhost --user=elexis --echo-queries --command="create database elexis_rcptt_$1 encoding 'utf8' template template0;"
+/usr/bin/env psql --host=localhost --user=elexis --echo-queries --command="drop database if exists elexis_rcptt_${language};"
+/usr/bin/env psql --host=localhost --user=elexis --echo-queries --command="create database elexis_rcptt_${language} encoding 'utf8' template template0;"
