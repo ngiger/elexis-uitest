a02a1bb6cfbd0988188e62a2333f6eaf3fc679e8
Niklaus Giger <niklaus.giger@member.fsf.org>
2021-04-16 21:32:51 +0200

    Updated install scripts for Debian and windows

--- vagrant/scripts/download_oomp_rcptt.sh
+++ vagrant/scripts/download_oomp_rcptt.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # https://linuxize.com/post/install-java-on-debian-10/
 set -o xtrace # enable seeing the commands, prepended with a + sign
+sudo userdel --force jenkins
 sudo useradd  --shell /usr/bin/fish --uid 1004 -G sudo,lp,users --create-home jenkins
 set -e # Do exit on any error:
 echo -e "elexisTest\nelexisTest\n" | sudo passwd jenkins
@@ -20,3 +21,10 @@ else
   wget --directory-prefix=/home/jenkins https://ftp.snt.utwente.nl/pub/software/eclipse/oomph/products/eclipse-inst-jre-linux64.tar.gz
 fi
 
+if test -f /home/jenkins/Apache_OpenOffice_4.1.9_Linux_x86-64_install-deb_de.tar.gz
+then
+  echo Apache_OpenOffic datei existiert
+else
+  echo Apache_OpenOffic datei fehlt
+  wget  --directory-prefix=/home/jenkins  https://netcologne.dl.sourceforge.net/project/openofficeorg.mirror/4.1.9/binaries/de/Apache_OpenOffice_4.1.9_Linux_x86-64_install-deb_de.tar.gz
+fi
--- vagrant/scripts/setup_elexis.sh
+++ vagrant/scripts/setup_elexis.sh
@@ -1,29 +1,33 @@
 #!/usr/bin/env bash
 # https://linuxize.com/post/install-java-on-debian-10/
 set -o xtrace # enable seeing the commands, prepended with a + sign
+sudo userdel --force jenkins
 sudo useradd  --shell /usr/bin/fish --uid 1004 -G sudo,lp,users --create-home jenkins
 set -e # Do exit on any error:
 source /etc/environment
 echo $JAVA_HOME
 echo -e "elexisTest\nelexisTest\n" | sudo passwd jenkins
 sudo -iHu jenkins <<EOF
-set -o xtrace # enable seeing the commands, prepended with a + sign
-mkdir .ssh
-echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJY7+iMHzZ4v7rfVdmpkMFzxwWwetpj9F/At/bo0OM1i jenkins Giger Dez 2019" > .ssh/authorized_keys
-unzip /home/jenkins/rcptt.ide-2.5.1-linux.gtk.x86_64.zip
+unzip -u /home/jenkins/rcptt.ide-2.5.1-linux.gtk.x86_64.zip
 pwd
 ls -lrt
-tar -zxvf /home/jenkins/eclipse-inst-jre-linux64.tar.gz
+tar -zxf /home/jenkins/eclipse-inst-jre-linux64.tar.gz
 EOF
 sudo cp /home/vagrant/scripts/jenkins-slave.service /home/vagrant/scripts/*.conf /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable jenkins-slave
 sudo systemctl start jenkins-slave
-sudo apt install libreoffice-writer imagemagick xvfb
-wget https://netcologne.dl.sourceforge.net/project/openofficeorg.mirror/4.1.9/binaries/de/
-Apache_OpenOffice_4.1.9_Linux_x86-64_install-deb_de.tar.gz
-tar -zxf Apache_OpenOffice_4.1.9_Linux_x86-64_install-deb_de.tar.gz
+sudo apt install -qy libreoffice-writer imagemagick xvfb
+tar -zxf /home/jenkins/Apache_OpenOffice_4.1.9_Linux_x86-64_install-deb_de.tar.gz
 sudo dpkg -i de/DEBS/*.deb
-sudo apt install -y firefox-esr-l10n-de konsole xterm okular postgresql mariadb-server mariadb-client
-
-sudo -u postgres psql
+sudo apt install -qy firefox-esr-l10n-de konsole xterm okular postgresql mariadb-server mariadb-client
+# taken from postgresql/pom.xml 
+sudo -u postgres psql <<EOF
+  create user elexis with password 'elexisTest';
+  create database elexis owner elexis encoding 'utf8' template template0;
+  ALTER USER elexis CREATEDB;
+  create database elexis_rcptt_de owner elexis encoding 'utf8' template template0;
+  GRANT ALL PRIVILEGES ON DATABASE elexis_rcptt_de TO elexis ;
+  GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO elexis;
+  ALTER DATABASE elexis_rcptt_de OWNER TO elexis;
+EOF
