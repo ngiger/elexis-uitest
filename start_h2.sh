80cab1936009772b38799978c105c7306835d7f6
Niklaus Giger <niklaus.giger@member.fsf.org>
2021-12-09 15:10:57 +0100

    [23454] Update view title Falldetail

--- start_h2.sh
+++ start_h2.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 os/Elexis3  -nl de_CH -os linux -ws gtk -arch x86_64 -consoleLog -vmargs \
 -Duser.language=de -Duser.region=CH -Dfile.encoding=utf-8 \
--Dch.elexis.dbFlavor=h2 -Dch.elexis.dbSpec='jdbc:h2:/home/niklaus/elexis/h2_elexis_rcptt_de/db;AUTO_SERVER=TRUE' \
--Dch.elexis.dbUser=sa -Dch.elexis.dbPw=  \
+-Dch.elexis.dbFlavor=h2 -Dch.elexis.dbSpec='jdbc:h2:/home/niklaus/elexis/h2_elexis_rcptt_de/db' \
+-Dch.elexis.dbUser=sa -Dch.elexis.dbPw= -Dch.elexis.dbH2AutoServer=1 \
 -Dch.elexis.firstMandantName=Mustermann -Dch.elexis.firstMandantPassword=elexisTest -Dch.elexis.firstMandantEmail=mmustermann@elexis.info \
 -Dch.elexis.username=Mustermann -Dch.elexis.password=elexisTest
