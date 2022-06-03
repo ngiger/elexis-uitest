582c63b83c9e8a46fc6a160275c255d97afb4dbb
Niklaus Giger <niklaus.giger@member.fsf.org>
2022-06-03 19:44:54 +0200

    Updated Vagrantfile for public network & jenkinslave

--- vagrant/scripts/setup_jenkins_slave.sh
+++ vagrant/scripts/setup_jenkins_slave.sh
@@ -1,22 +1,26 @@
 #!/usr/bin/env bash
 # https://linuxize.com/post/install-java-on-debian-10/
 set -o xtrace # enable seeing the commands, prepended with a + sign
-sudo apt install -qy mariadb-server
+export DEBIAN_FRONTEND=noninteractive
 sudo userdel --force jenkins_slave
 sudo useradd  --shell /usr/bin/fish --uid 1007 -G sudo,lp,users --create-home jenkins_slave
 set -e # Do exit on any error:
 source /etc/environment
 echo $JAVA_HOME
 echo -e "elexisTest\nelexisTest\n" | sudo passwd jenkins_slave
+# Prepare jenkins_slave
 sudo -iHu jenkins_slave <<EOF
+rm -f agent.jar
 wget -nv https://srv.elexis.info/jenkins/jnlpJars/agent.jar
+mkdir -p ~/.ssh ~/slave/remoting ~/slave/workspace
+echo 'JS2a78HHzBNm73R6AA5uO8hofjqm2k69mbO3alQZkpUmvXyjXe9CZDaze4arYrL9wnRLaOi5HQ9s4lls9ROOBUo5J6YeHFtmpQ4sMWEs4IrMy+QXlmpNrK97N9GY3nhKYvvP2M5+yUajWx3yuiRF0CqQmu9jbBJFMnHW4XOKD jenkins@srv' | tee --append ~/.ssh/authorized_keys
 EOF
 
-sudo apt install -qy mariadb-common mysql-common
+sudo apt-get install -qy mariadb-common mysql-common
 sudo mkdir -p /etc/mysql/mariadb.conf.d/
 echo "[mariadb]" | sudo tee /etc/mysql/mariadb.conf.d/lowercase.cnf
 echo "lower_case_table_names=1" | sudo tee --append /etc/mysql/mariadb.conf.d//lowercase.cnf
-sudo apt install -qy mariadb-server
+sudo apt-get install -qy mariadb-server postgresql
 sudo -u root mysql -e 'SHOW VARIABLES LIKE "%case%";'
 sudo -u root mysql -e 'SHOW VARIABLES LIKE "%lower_case_table_names%";' | grep -w 1
 
@@ -32,9 +36,24 @@ sudo -u root mysql <<EOF
     flush privileges;
 EOF
 
-sudo cp /home/vagrant/scripts/jenkins-slave.service /home/vagrant/scripts/*.conf /etc/systemd/system/
-sudo systemctl daemon-reload
-sudo systemctl enable jenkins-slave
-sudo systemctl start jenkins-slave
-sleep 2
-sudo systemctl status jenkins-slave
+# taken from postgresql/pom.xml
+sudo -u postgres psql <<EOF
+  create user elexis with password 'elexisTest';
+  create database elexis owner elexis encoding 'utf8' template template0;
+  ALTER USER elexis CREATEDB;
+  create database elexis_rcptt_de owner elexis encoding 'utf8' template template0;
+  GRANT ALL PRIVILEGES ON DATABASE elexis_rcptt_de TO elexis ;
+  GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO elexis;
+  ALTER DATABASE elexis_rcptt_de OWNER TO elexis;
+  create database elexis_rcptt_fr owner elexis encoding 'utf8' template template0;
+  GRANT ALL PRIVILEGES ON DATABASE elexis_rcptt_fr TO elexis ;
+  GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO elexis;
+  ALTER DATABASE elexis_rcptt_fr OWNER TO elexis;
+EOF
+
+# sudo cp /home/vagrant/scripts/jenkins-slave.service /home/vagrant/scripts/*.conf /etc/systemd/system/
+# sudo systemctl daemon-reload
+# sudo systemctl enable jenkins-slave
+# sudo systemctl start jenkins-slave
+# sleep 2
+# sudo systemctl status jenkins-slave
