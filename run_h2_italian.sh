08f4e92c54cf9084f2f408c8fd72a7a96beb5093
Niklaus Giger <niklaus.giger@member.fsf.org>
2022-07-30 10:33:31 +0200

    Some small, but important fixes

--- run_h2_italian.sh
+++ run_h2_italian.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
-set +v
 logger "cron: $0 started"
 cd `dirname $0`
 # other useful definitions are:
 # -DautExplicit=/opt/rm/elexis-master/elexis-3-core/ch.elexis.core.p2site/target/products/ch.elexis.core.application.ElexisApp-[classifier].zip
 # -Dsuite2run=QuickTestSuite
 # -DuseBranch=f11106
-xvfb-run -a openbox --startup "mvn -V --batch-mode clean install --settings $PWD/h2/settings.xml -f $PWD/postgresql/pom.xml -Dlanguage=it" 2>&1 | tee `basename --suffix=.sh $0`.log
-  
+export cmd="mvn -V --batch-mode clean install -f $PWD/h2/pom.xml -Dlanguage=it --settings=$PWD/h2/settings.xml -DautExplicit=/home/niklaus/work -Dsuite2run=SmokeTestSetup"
+export cmd="mvn -V --batch-mode clean install -f $PWD/h2/pom.xml -Dlanguage=it --settings=$PWD/h2/settings.xml "
+# $cmd 2>&1 | tee `basename --suffix=.sh $0`.log
+xvfb-run -a openbox --startup "$cmd" 2>&1 | tee `basename --suffix=.sh $0`.log
