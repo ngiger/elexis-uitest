b5293bcdbfc68d420a41ef5dd8296b90e2be5f58
Niklaus Giger <niklaus.giger@member.fsf.org>
2022-07-31 14:06:28 +0200

    Fix running postgresql in french or italian

--- postgresql/drop_create_db.sh
+++ postgresql/drop_create_db.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 # Created by elexis-uitest/dbSpec/pom.xml as /home/niklaus/medelexis-uitest/postgresql/../postgresql/drop_create_db.sh
 export PGPASSWORD=elexisTest
-/usr/bin/env psql --host=localhost --user=elexis --command="drop database if exists elexis_rcptt_de;"
-/usr/bin/env psql --host=localhost --user=elexis --command="create database elexis_rcptt_de encoding 'utf8' template template0;"
+language=${1:-de}
+echo $0: language ${language}
+/usr/bin/env psql --host=localhost --user=elexis --command="drop database if exists elexis_rcptt_${language};"
+/usr/bin/env psql --host=localhost --user=elexis --command="create database elexis_rcptt_${language} encoding 'utf8' template template0;"
